fb0aa58f9484f007999eb29ff096a3566a25de40
Thanh Le <thanhlevan0203@gmail.com>
2022-06-23 22:10:28 -0700

    Created navbar and components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;

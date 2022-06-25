da129c47a58188d79d6074a1e301dc57930763a3
Thanh Le <thanhlevan0203@gmail.com>
2022-06-25 15:38:53 -0700

    Created Header and SubHeading Page

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Header.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='app__header app__wrapper section__padding' id='home'>
+      <div className='app__wrapper_info'>
+        <SubHeading title="Chase the new flavor"/>
+        <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>The food at a fine dining establishment will be of the highest quality, made with luxurious ingredients, and served in unique and beautiful presentations. The quality of the dishware and cutlery is also higher, and often the décor and even the architecture of the restaurant building is notable</p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.welcome} alt='header img'/>
+
+      </div>
     </div>
   )
 }

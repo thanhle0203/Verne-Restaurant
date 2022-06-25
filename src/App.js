3140040d5cba38507b5f2193a26bb37666fa7274
Thanh Le <thanhlevan0203@gmail.com>
2022-06-25 15:08:27 -0700

    Worked on Navbar menu

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from 'react';
 import { Navbar } from './components';
 import { Header, AboutUs, SpecialMenu, Chef, Intro, Laurels, Gallery, FindUs, Footer } from './container';
 import './App.css';
+import { BrowserRouter as Router, Switch, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      App
+    <Router>
       <Navbar />
       <Header />
-      <AboutUs />
-      <SpecialMenu />
-      <Chef />
-      <Intro />
-      <Laurels />
-      <Gallery />
-      <FindUs />
-      <Footer />
 
-    </div>
+    </Router>
   )
 }
 

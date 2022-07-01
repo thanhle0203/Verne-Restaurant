212c70036254fae3129d0e803a69dffc141635b0
Thanh Le <thanhlevan0203@gmail.com>
2022-06-30 21:43:32 -0700

    Created Footer Component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => (
     <Laurels />
     <Gallery />
     <FindUs />
+    <Footer />
   </Router>
     
 )
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
 
-const Newsletter = () => {
-  return (
-    <div>
-      Newsletter
+const Newsletter = () => (
+    <div className='app__newsletter'>
+      <div className='app__newsletter-heading'>
+        <SubHeading title='Newsletter' />
+        <h1 className='headtext__cormorant'>Subcribe To Our Newsletter</h1>
+        <p className='p__opensans'>And never miss latest Updates!</p>
+      </div>
+      <div className='app__newsletter-input flex__center'>
+        <input type='email' placeholder='Enter your email address'/>
+        <button className='custom_bottom'>Subscribe</button>
+      </div>
     </div>
-  )
-}
+  
+);
 
 export default Newsletter;

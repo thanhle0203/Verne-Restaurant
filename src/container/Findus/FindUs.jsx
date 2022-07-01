6c091658405205320ce768fd8ce1c71b43202010
Thanh Le <thanhlevan0203@gmail.com>
2022-06-30 18:54:44 -0700

    Created FindUs component

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
+import { SubHeading } from '../../components'; 
+import { images } from '../../constants'; 
+
 const FindUs = () => {
   return (
-    <div>FindUs</div>
+    <div className='app__bg app__wrapper section__padding' id='contact'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Contact'/>
+        <h1 className='headtext__cormorant' style={{ marginBottom: '3rem'}}>
+          FindUs
+        </h1>
+        <div className='app__wrapper-content'>
+          <p className='p__opensans'>Located on the 2nd floor of the Eiffel tower, Paris, France.</p>
+          <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+          <p className='p__opensans'>Mon - Fri: 10:00 am - 09:00 pm</p>
+          <p className='p__opensans'>Sat - Sun: 10:00 am - 10:00 pm</p>
+
+        </div>
+
+        <div type='button' className='custom__button' style={{marginTop: '2rem'}}>Visit Us</div>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.findus} alt='findus'/>
+      </div>
+    </div>
   )
 }
 

bf86280dbda91e44b3960e3bed77a7910668f337
Thanh Le <thanhlevan0203@gmail.com>
2022-06-30 22:49:59 -0700

    New updated Footer component

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,13 +3,11 @@ import './Footer.css';
 import { FooterOverlay, Newsletter } from '../../components';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
-const Footer = () => {
-  return (
+const Footer = () => (
     <div className='app__footer section__padding'>
       <FooterOverlay />
       <Newsletter />
     </div>
-  )
-}
+);
 
 export default Footer
